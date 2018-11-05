f19a62cda5fc3a194acf84dd050beaffaa725f01
KarenPudner <karen.pudner@bbc.co.uk>
2018-11-05 11:31:15 +0000

    Can filter out films with below average ratings per customer

--- recommender/ratings_converter.py
+++ recommender/ratings_converter.py
@@ -3,3 +3,12 @@ import pandas as pd
 def find_average_rating_per_customer(ratings):
     average_ratings_per_customer = ratings.groupby(['CustomerId'])['Rating'].mean()
     return average_ratings_per_customer
+
+def convert_customer_rating_to_film_list(ratings):
+    grouped_by = ratings.groupby(['CustomerId'])
+    print("head")
+    print(grouped_by.head())
+    grouped_by=grouped_by.apply(lambda g: g[g['Rating'] >= g['Rating'].mean()])
+    print("head filtered")
+    print(grouped_by.head())
+    return grouped_by
--- test/test_ratings_converter.py
+++ test/test_ratings_converter.py
@@ -7,6 +7,25 @@ def test_average_rating():
              'Rating': [4,3,2,4,5,3],
              'TimeStamp': [978302109,978302109,978302109,978302109,978302109,978302109]}
 
+    df = pd.DataFrame.from_dict(ratings)
     average = ratings_converter.find_average_rating_per_customer(df)
     assert average[0]==3
     assert average[1] == 4
+
+
+def test_convert_ratings_to_film_list():
+    ratings = {'CustomerId': ['1', '1', '1','2','2','2'],
+             'FilmId': ['4', '5', '6','20', '21','22'],
+             'Rating': [4,3,2,4,5,3],
+             'TimeStamp': [978302109,978302109,978302109,978302109,978302109,978302109]}
+
+    df = pd.DataFrame.from_dict(ratings)
+
+    average = ratings_converter.find_average_rating_per_customer(df)
+    converted_list=ratings_converter.convert_customer_rating_to_film_list(df)
+    expected = {
+        '1': ['4', '5'],
+        '2': ['20','21']
+    }
+
+    assert converted_list.shape==(4,4)
