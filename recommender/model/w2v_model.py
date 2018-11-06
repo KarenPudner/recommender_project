71ab03a9ce1ffec25993620d84a5374ef59ee6a2
KarenPudner <karen.pudner@bbc.co.uk>
2018-11-06 11:23:25 +0000

    Can spilt into history and groundtruth and get embeddings for history

--- recommender/model/w2v_model.py
+++ recommender/model/w2v_model.py
@@ -1,4 +1,5 @@
 from gensim.models import Word2Vec
+import numpy as np
 
 class W2VModel:
     model = None
@@ -7,11 +8,44 @@ class W2VModel:
         pass
 
     def train_model(self,lists_of_films):
-        self.model = Word2Vec(lists_of_films,size=100, window=5, min_count=1, workers=4)
+        self.model = Word2Vec(lists_of_films, size=20, window=5, min_count=1)
 
     def get_model_vocabulary(self):
         return self.model.wv.vocab
 
 
+    def evaluate(self, playlists):
+        for playlist in playlists:
+
+            user_history, groundtruth = self.split_playlist_in_history_and_groundtruth(self,playlist)
+            user_embedding = self.generate_user_embedding(user_history)
+    #     if user_embedding is not None:
+    #         recommended_items = predict(m, user_embedding, user_history, LIST_LENGTH)
+    #         scores.update_all_metrics(groundtruth, recommended_items)
+    #
+    # return scores.get_metrics()
+
+    def split_playlist_in_history_and_groundtruth(self,playlist):
+
+        # If number of items odd, then give slightly more to the user history
+        number_of_items_in_history = int(np.ceil(len(playlist)*0.5))
+        history_items = playlist[:number_of_items_in_history]
+        groundtruth_items = playlist[number_of_items_in_history:]
+
+        return history_items, groundtruth_items
+
+    def convert_history_items_to_vectors(self, history_items):
+        # Also only consider items that are featured in the model vocabulary
+        return [self.model.wv[v] for v in history_items if v in self.model.wv.vocab]
 
 
+    def generate_user_embedding(self, user_history):
+        item_embeddings = self.convert_history_items_to_vectors(user_history)
+        if len(item_embeddings) > 0:
+            em = np.mean(item_embeddings, axis=0)
+            if np.isnan(em).any():
+                print("Problem")
+                return em
+
+        else:
+            return None
--- test/model/test_w2v_model.py
+++ test/model/test_w2v_model.py
@@ -2,8 +2,28 @@ from recommender.model.w2v_model import W2VModel
 
 SENTENCES = [['cat', 'say', 'meow'], ['dog', 'say', 'woof']]
 
+
 def  test_W2VModel_train():
     model = W2VModel()
     model.train_model(SENTENCES)
     vocab=model.get_model_vocabulary()
     assert len(vocab)==5
+
+def test_split_playlist_in_history_and_groundtruth():
+    model = W2VModel()
+    even_list=["a","b","c","d"]
+    odd_list=["e","f","g","h","i"]
+    even_history, even_groundtruth = model.split_playlist_in_history_and_groundtruth(even_list)
+    assert even_history ==["a","b"]
+    assert even_groundtruth == ["c", "d"]
+    odd_history, odd_groundtruth = model.split_playlist_in_history_and_groundtruth(odd_list)
+    assert odd_history == ["e", "f", "g"]
+    assert odd_groundtruth == ["h", "i"]
+
+def test_convert_history_items_to_vectors():
+    history_items = ['cat', 'dog','rabbit']
+    model = W2VModel()
+    model.train_model(SENTENCES)
+    item_embeddings = model.convert_history_items_to_vectors(history_items)
+    assert len(item_embeddings) == 2
+    assert len(item_embeddings[0]) == 20
