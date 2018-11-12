a799d79e3ddde2d604ae4347b1b5680417a56ecc
KarenPudner <karen.pudner@bbc.co.uk>
2018-11-12 11:08:34 +0000

    Split up embedding process to make more understandable

--- recommender/model/w2v_model.py
+++ recommender/model/w2v_model.py
@@ -7,23 +7,23 @@ class W2VModel:
     def __init__(self):
         pass
 
-    def train_model(self,lists_of_films):
-        self.model = Word2Vec(lists_of_films, size=20, window=5, min_count=1)
+    def train_model(self,lists_of_films, vector_dimension):
+        self.model = Word2Vec(lists_of_films, size=vector_dimension, window=5, min_count=1)
 
     def get_model_vocabulary(self):
         return self.model.wv.vocab
 
 
-    def evaluate(self, playlists):
-        for playlist in playlists:
-
-            user_history, groundtruth = self.split_playlist_in_history_and_groundtruth(self,playlist)
-            user_embedding = self.generate_user_embedding(user_history)
-    #     if user_embedding is not None:
-    #         recommended_items = predict(m, user_embedding, user_history, LIST_LENGTH)
-    #         scores.update_all_metrics(groundtruth, recommended_items)
+    # def evaluate(self, playlists):
+    #     for playlist in playlists:
     #
-    # return scores.get_metrics()
+    #         user_history, groundtruth = self.split_playlist_in_history_and_groundtruth(self,playlist)
+    #         user_embedding = self.generate_user_embedding(user_history)
+    #     #     if user_embedding is not None:
+    # #         recommended_items = predict(m, user_embedding, user_history, LIST_LENGTH)
+    # #         scores.update_all_metrics(groundtruth, recommended_items)
+    # #
+    # # return scores.get_metrics()
 
     def split_playlist_in_history_and_groundtruth(self,playlist):
 
@@ -34,18 +34,38 @@ class W2VModel:
 
         return history_items, groundtruth_items
 
-    def convert_history_items_to_vectors(self, history_items):
+    def retrieve_input_and_output_vectors_for_item(self, history_items):
         # Also only consider items that are featured in the model vocabulary
         return [self.model.wv[v] for v in history_items if v in self.model.wv.vocab]
 
-
-    def generate_user_embedding(self, user_history):
-        item_embeddings = self.convert_history_items_to_vectors(user_history)
-        if len(item_embeddings) > 0:
-            em = np.mean(item_embeddings, axis=0)
-            if np.isnan(em).any():
-                print("Problem")
+    def aggregate_item_input_and_output_vectors(self, item_vectors, method='average'):
+        if len(item_vectors) > 0:
+            if method == 'average':
+                em = np.mean(item_vectors, axis=0)
+                if np.isnan(em).any():
+                    print("Problem")
                 return em
-
+            elif method == 'sum':
+                return np.sum(item_vectors, axis=0)
+            else:
+                raise TypeError("Method must be specified as either 'average' or 'sum'")
         else:
             return None
+
+
+    def generate_user_embedding(self, user_history, method='average'):
+        item_embeddings = self.retrieve_input_and_output_vectors_for_item(user_history)
+        return self.aggregate_item_input_and_output_vectors(item_embeddings, method)
+
+    def predict(self, user_embedding, user_history, k):
+        rec_list = list()
+        kk = k
+
+        # Keep generating recommendations until required number is achieved while
+        # discounting all recommendations that have been seen.
+        while len(rec_list) < k and kk < len(self.model.wv.vocab):
+            recommended_items = self.model.wv.most_similar(positive=[user_embedding], topn=kk)
+            rec_list = [i[0] for i in recommended_items if i[0] not in user_history]
+            kk += 1
+
+        return rec_list
--- test/model/test_w2v_model.py
+++ test/model/test_w2v_model.py
@@ -1,11 +1,13 @@
 from recommender.model.w2v_model import W2VModel
+import numpy as np
+from numpy.testing import assert_array_equal, assert_allclose
 
 SENTENCES = [['cat', 'say', 'meow'], ['dog', 'say', 'woof']]
 
 
 def  test_W2VModel_train():
     model = W2VModel()
-    model.train_model(SENTENCES)
+    model.train_model(SENTENCES,2)
     vocab=model.get_model_vocabulary()
     assert len(vocab)==5
 
@@ -20,10 +22,61 @@ def test_split_playlist_in_history_and_groundtruth():
     assert odd_history == ["e", "f", "g"]
     assert odd_groundtruth == ["h", "i"]
 
-def test_convert_history_items_to_vectors():
-    history_items = ['cat', 'dog','rabbit']
+def test_convert_history_items_to_input_and_output_vectors():
+    history_items = ['cat', 'dog', 'rabbit']
     model = W2VModel()
-    model.train_model(SENTENCES)
-    item_embeddings = model.convert_history_items_to_vectors(history_items)
+    vector_dimension=3
+    model.train_model(SENTENCES, vector_dimension)
+    item_embeddings = model.retrieve_input_and_output_vectors_for_item(history_items)
     assert len(item_embeddings) == 2
-    assert len(item_embeddings[0]) == 20
+    assert len(item_embeddings[0]) == 3
+
+def test_average_item_input_and_item_vectors():
+    input_vector = [1,2,3,4]
+    output_vector = [5,6,7,8]
+    model = W2VModel()
+    actual_averaged_vector = model.aggregate_item_input_and_output_vectors([input_vector,output_vector], 'average')
+    expected_averaged_vector = np.array([3.0, 4.0, 5.0, 6.0])
+    assert actual_averaged_vector[0] ==expected_averaged_vector[0]
+    assert actual_averaged_vector[1] == expected_averaged_vector[1]
+    assert actual_averaged_vector[2] == expected_averaged_vector[2]
+    assert actual_averaged_vector[3] == expected_averaged_vector[3]
+
+def test_sum_item_input_and_item_vectors():
+    input_vector = [1,2,3,4]
+    output_vector = [5,6,7,8]
+    model = W2VModel()
+    actual_averaged_vector = model.aggregate_item_input_and_output_vectors([input_vector,output_vector], 'sum')
+    expected_averaged_vector = np.array([6.0, 8.0, 10.0, 12.0])
+    assert actual_averaged_vector[0] ==expected_averaged_vector[0]
+    assert actual_averaged_vector[1] == expected_averaged_vector[1]
+    assert actual_averaged_vector[2] == expected_averaged_vector[2]
+    assert actual_averaged_vector[3] == expected_averaged_vector[3]
+
+
+def test_generate_user_embedding():
+    history_items = ['cat', 'dog', 'rabbit']
+    model = W2VModel()
+    vector_dimension = 3
+    model.train_model(SENTENCES, vector_dimension)
+    item_embeddings_average= model.generate_user_embedding(history_items, 'average')
+    assert len(item_embeddings_average) == 3
+
+
+def test_predict():
+    model = W2VModel()
+    vector_dimension = 3
+    model.train_model(SENTENCES, vector_dimension)
+    cat_vector =  model.generate_user_embedding(['cat'])
+    history = ['dog']
+    predict = model.predict(cat_vector, history, 1)
+    assert predict == ['cat']
+
+
+def test_predict_matching_user_history():
+    model = W2VModel()
+    vector_dimension = 3
+    model.train_model(SENTENCES, vector_dimension)
+    cat_vector = model.generate_user_embedding(['cat'])
+    history = ['cat']
+    assert ['cat'] not in model.predict(cat_vector, history, 1)
